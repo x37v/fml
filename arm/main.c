6a99487f4a5d8028b72727fe3a7b280f6a43e843
Alex Norman <x37v.alex@gmail.com>
2016-01-20 20:23:07 -0800

    quick and dirty, leds now reflect button state

--- arm/main.c
+++ arm/main.c
@@ -9,27 +9,69 @@ volatile uint32_t time_var1, time_var2;
 void Delay(volatile uint32_t nCount);
 void init();
 
+struct io_mapping {
+  GPIO_TypeDef* port;
+  uint16_t pin;
+};
+
+struct io_mapping buttons[3] = {
+  {GPIOE, GPIO_Pin_8},
+  {GPIOE, GPIO_Pin_9},
+  {GPIOE, GPIO_Pin_11}
+};
+
+struct io_mapping leds[4] = {
+  {GPIOB, GPIO_Pin_2},
+  {GPIOE, GPIO_Pin_7},
+  {GPIOE, GPIO_Pin_10},
+  {GPIOE, GPIO_Pin_12}
+};
+
 int main(void) {
   init();
 
   for(;;) {
-    GPIO_SetBits(GPIOB, GPIO_Pin_2);
-    GPIO_SetBits(GPIOE, GPIO_Pin_7);
-    GPIO_SetBits(GPIOE, GPIO_Pin_10);
-    GPIO_SetBits(GPIOE, GPIO_Pin_12);
-    Delay(500);
-    GPIO_ResetBits(GPIOB, GPIO_Pin_2);
-    GPIO_ResetBits(GPIOE, GPIO_Pin_7);
-    GPIO_ResetBits(GPIOE, GPIO_Pin_10);
-    GPIO_ResetBits(GPIOE, GPIO_Pin_12);
-    Delay(500);
+    for (int i = 0; i < 3; i++) {
+      if (GPIO_ReadInputDataBit(buttons[i].port, buttons[i].pin)) {
+        GPIO_SetBits(leds[i].port, leds[i].pin);
+      } else {
+        GPIO_ResetBits(leds[i].port, leds[i].pin);
+      }
+    }
+    Delay(10);
   }
 
   return 0;
 }
 
+void setup_leds() {
+  GPIO_InitTypeDef GPIO_InitStructure;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+
+  for (int i = 0; i < 4; i++) {
+    GPIO_InitStructure.GPIO_Pin = leds[i].pin;
+    GPIO_Init(leds[i].port, &GPIO_InitStructure);
+    GPIO_ResetBits(leds[i].port, leds[i].pin);
+  }
+}
+
+void setup_buttons() {
+  GPIO_InitTypeDef GPIO_InitStructure;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+
+  for (int i = 0; i < 3; i++) {
+    GPIO_InitStructure.GPIO_Pin = buttons[i].pin;
+    GPIO_Init(buttons[i].port, &GPIO_InitStructure);
+  }
+}
+
 void init() {
-  GPIO_InitTypeDef  GPIO_InitStructure;
 
   // ---------- SysTick timer -------- //
   if (SysTick_Config(SystemCoreClock / 1000)) {
@@ -42,21 +84,8 @@ void init() {
   RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
   RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);
 
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
-
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
-  GPIO_Init(GPIOB, &GPIO_InitStructure);
-
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7 | GPIO_Pin_10 | GPIO_Pin_12;
-  GPIO_Init(GPIOE, &GPIO_InitStructure);
-
-  GPIO_SetBits(GPIOB, GPIO_Pin_2);
-  GPIO_SetBits(GPIOE, GPIO_Pin_7);
-  GPIO_SetBits(GPIOE, GPIO_Pin_10);
-  GPIO_SetBits(GPIOE, GPIO_Pin_12);
+  setup_leds();
+  setup_buttons();
 }
 
 void SysTick_Handler(void)
