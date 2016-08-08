4cc7059c7c739ab049bbac01b9ba398ebae65da7
Alex Norman <x37v.alex@gmail.com>
2016-08-08 13:21:28 -0700

    check for zero in lin smooth

--- src/util.cpp
+++ src/util.cpp
@@ -2,6 +2,13 @@
 #include <cmath>
 
 float fm::lin_smooth(float target, float current, float increment) {
+#if 0
+  return target;
+#else
+  //XXX shouldn't happen
+  if (increment == 0)
+    return target;
+
   if (increment > 0) {
     if (target <= current)
       return target;
@@ -9,7 +16,7 @@ float fm::lin_smooth(float target, float current, float increment) {
     if (target >= current)
       return target;
   }
- 
   return current + increment;
+#endif
 }
 
